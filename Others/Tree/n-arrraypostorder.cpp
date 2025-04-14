98e470c464edfaeee1e7e9e2afddf2a7e99ccf3d
Abhishek Thakur <abhishekthakurn0@gmail.com>
2025-04-14 23:59:46 +0530

    local fix

--- Others/Tree/n-arrraypostorder.cpp
+++ Others/Tree/n-arrraypostorder.cpp
@@ -1,27 +1,77 @@
+#include <iostream>
+#include <vector>
+#include <stack>
 
+using namespace std;
+
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
 
 class Solution {
-    public:
-        vector<int> postorder(Node* root) {
-            vector<int> result;
-            if (!root) {
-                return result;
-            }
-    
-            stack<Node*> nodes;
-            nodes.push(root);
-    
-            while (!nodes.empty()) {
-                Node* current = nodes.top();
-                nodes.pop();
-    
-                result.insert(result.begin(), current->val);
-    
-                for (Node* child : current->children) {
-                    nodes.push(child);
-                }
-            }
-    
+public:
+    vector<int> postorder(Node* root) {
+        vector<int> result;
+        if (!root) {
             return result;
         }
-    };
+
+        stack<Node*> nodes;
+        nodes.push(root);
+
+        while (!nodes.empty()) {
+            Node* current = nodes.top();
+            nodes.pop();
+
+            result.insert(result.begin(), current->val);
+
+            for (Node* child : current->children) {
+                nodes.push(child);
+            }
+        }
+
+        return result;
+    }
+};
+
+// Example usage
+int main() {
+    Node* child1 = new Node(5);
+    Node* child2 = new Node(6);
+    vector<Node*> children = {child1, child2};
+    Node* root = new Node(1, {new Node(3, children), new Node(2), new Node(4)});
+
+    Solution sol;
+    vector<int> result = sol.postorder(root);
+
+    for (int val : result) {
+        cout << val << " ";
+    }
+    cout << endl;
+
+    // Clean up memory
+    delete child1;
+    delete child2;
+    delete root->children[0]->children[0];
+    delete root->children[0]->children[1];
+    delete root->children[0];
+    delete root->children[1];
+    delete root->children[2];
+    delete root;
+
+    return 0;
+}
--- Others/Tree/treetraversal.cpp
+++ Others/Tree/treetraversal.cpp
@@ -1,14 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
 /**
  * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
  */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
 class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
@@ -16,25 +24,53 @@ public:
         if (!root) {
             return result;
         }
-        
+
         stack<TreeNode*> nodes;
         nodes.push(root);
-        
+
         while (!nodes.empty()) {
             TreeNode* current = nodes.top();
             nodes.pop();
-            
+
             result.push_back(current->val);
-            
+
             if (current->right) {
                 nodes.push(current->right);
             }
-            
+
             if (current->left) {
                 nodes.push(current->left);
             }
         }
-        
+
         return result;
     }
 };
+
+// Example usage
+int main() {
+    // Create a simple binary tree:
+    //     1
+    //      \
+    //       2
+    //      /
+    //     3
+    TreeNode* node3 = new TreeNode(3);
+    TreeNode* node2 = new TreeNode(2, node3, nullptr);
+    TreeNode* root = new TreeNode(1, nullptr, node2);
+
+    Solution sol;
+    vector<int> result = sol.preorderTraversal(root);
+
+    for (int val : result) {
+        cout << val << " ";
+    }
+    cout << endl;
+
+    // Cleanup
+    delete node3;
+    delete node2;
+    delete root;
+
+    return 0;
+}

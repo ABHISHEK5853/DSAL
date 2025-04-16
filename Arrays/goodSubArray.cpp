7bf6953badcb4c2877bd00ba65b3da5793ffde88
Abhishek Thakur <abhishekthakurn0@gmail.com>
2025-04-16 23:41:30 +0530

    Dpp's LC Daily

---
+++ Arrays/goodSubArray.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <unordered_map> 
+
+
+using namespace std;
+class Solution {
+    public:
+        long long countGood(vector<int>& nums, int k) {
+            unordered_map<int, int> freq;
+            long long pairs = 0, res = 0;
+            int left = 0;
+    
+            for (int right = 0; right < nums.size(); ++right) {
+                pairs += freq[nums[right]];
+                freq[nums[right]]++;
+    
+                while (pairs >= k) {
+                    res += nums.size() - right;
+    
+                    freq[nums[left]]--;
+                    pairs -= freq[nums[left]];
+                    left++;
+                }
+            }
+    
+            return res;
+        }
+    };
+    
